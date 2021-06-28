65c832fdb94bee0df445a2509755f5fcebe56bed
Jacopoguerini <jacopo.guerini1993@gmail.com>
2021-06-28 16:47:01 +0200

    Snack 3 completato, Snack 2 in dubbio

--- snack-2/index.php
+++ snack-2/index.php
@@ -1,3 +1,23 @@
 <!-- Snack 2
 Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
  -->
+
+<?php 
+
+    
+    $parameters = [
+        $name = $_GET["name"],
+        $mail = $_GET["mail"],
+        $age = $_GET["age"]
+    ];
+
+    for ($i; $i <= count($parameters); $i++) {
+        if (strlen($name) > 3 && str_contains($mail, '.') && str_contains($mail, '@') && is_numeric($age) == true)  {
+            echo "Accesso riuscito";
+        } else {
+            echo "Accesso negato";
+        }
+    }
+
+ ?>
+ 
--- snack-3/index.php
+++ snack-3/index.php
@@ -1,3 +1,20 @@
 <!-- Snack 3
 Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta
  -->
+
+<?php 
+
+    $array = [];
+
+    for ($i = 0; count($array) < 15; $i++) {
+        $n = rand(1, 100);
+        if (in_array($n, $array) == false) {
+            array_push($array, $n);
+        }
+    }
+
+    echo "<pre>";
+    var_dump($array);
+    echo "</pre>";
+
+?>
