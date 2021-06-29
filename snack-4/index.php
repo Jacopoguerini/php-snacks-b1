b903345f60738d5f36a8bc30e42aa0f45cbc76f8
Jacopoguerini <jacopo.guerini1993@gmail.com>
2021-06-29 12:13:13 +0200

    Snack 4 completato

---
+++ snack-4/index.php
@@ -0,0 +1,57 @@
+<!-- Snack #4
+Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data.
+Stampare ogni data con i relativi post. -->
+
+<?php
+
+    $posts = [
+
+        '10/01/2019' => [
+            [
+                'title' => 'Post 1',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 1'
+            ],
+            [
+                'title' => 'Post 2',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 2'
+            ],
+        ],
+        '10/02/2019' => [
+            [
+                'title' => 'Post 3',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 3'
+            ]
+        ],
+        '15/05/2019' => [
+            [
+                'title' => 'Post 4',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 4'
+            ],
+            [
+                'title' => 'Post 5',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 5'
+            ],
+            [
+                'title' => 'Post 6',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 6'
+            ]
+        ]
+    ];
+
+    foreach ($posts as $key => $value) {
+        echo "<div>";
+        echo "<h4>" . $key . "</h4>";
+            foreach ($value as $post) {
+                echo "<h1>" . $post["title"] . "</h1>";
+                echo "<h3>" . $post["author"] . "</h3>";
+                echo "<p>" . $post["text"] . "</p>";
+            }
+        echo "</div>";
+    }
+?>
