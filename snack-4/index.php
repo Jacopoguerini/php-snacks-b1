4fb9a7484ecf3addc47e875b37d38373f6e4391f
Jacopoguerini <jacopo.guerini1993@gmail.com>
2021-06-29 12:34:44 +0200

    Inserimento php in html

--- snack-4/index.php
+++ snack-4/index.php
@@ -2,56 +2,68 @@
 Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data.
 Stampare ogni data con i relativi post. -->
 
-<?php
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Snak 4</title>
+    </head>
+    <body>
 
-    $posts = [
+        <?php
+        $posts = [
 
-        '10/01/2019' => [
-            [
-                'title' => 'Post 1',
-                'author' => 'Michele Papagni',
-                'text' => 'Testo post 1'
+            '10/01/2019' => [
+                [
+                    'title' => 'Post 1',
+                    'author' => 'Michele Papagni',
+                    'text' => 'Testo post 1'
+                ],
+                [
+                    'title' => 'Post 2',
+                    'author' => 'Michele Papagni',
+                    'text' => 'Testo post 2'
+                ],
             ],
-            [
-                'title' => 'Post 2',
-                'author' => 'Michele Papagni',
-                'text' => 'Testo post 2'
+            '10/02/2019' => [
+                [
+                    'title' => 'Post 3',
+                    'author' => 'Michele Papagni',
+                    'text' => 'Testo post 3'
+                ]
             ],
-        ],
-        '10/02/2019' => [
-            [
-                'title' => 'Post 3',
-                'author' => 'Michele Papagni',
-                'text' => 'Testo post 3'
+            '15/05/2019' => [
+                [
+                    'title' => 'Post 4',
+                    'author' => 'Michele Papagni',
+                    'text' => 'Testo post 4'
+                ],
+                [
+                    'title' => 'Post 5',
+                    'author' => 'Michele Papagni',
+                    'text' => 'Testo post 5'
+                ],
+                [
+                    'title' => 'Post 6',
+                    'author' => 'Michele Papagni',
+                    'text' => 'Testo post 6'
+                ]
             ]
-        ],
-        '15/05/2019' => [
-            [
-                'title' => 'Post 4',
-                'author' => 'Michele Papagni',
-                'text' => 'Testo post 4'
-            ],
-            [
-                'title' => 'Post 5',
-                'author' => 'Michele Papagni',
-                'text' => 'Testo post 5'
-            ],
-            [
-                'title' => 'Post 6',
-                'author' => 'Michele Papagni',
-                'text' => 'Testo post 6'
-            ]
-        ]
-    ];
+        ];
 
-    foreach ($posts as $key => $value) {
-        echo "<div>";
-        echo "<h4>" . $key . "</h4>";
-            foreach ($value as $post) {
-                echo "<h1>" . $post["title"] . "</h1>";
-                echo "<h3>" . $post["author"] . "</h3>";
-                echo "<p>" . $post["text"] . "</p>";
-            }
-        echo "</div>";
-    }
-?>
+        foreach ($posts as $date => $datePosts) { ?>
+            <section>
+                <h2><?php echo $date; ?></h2>
+                <?php foreach ($datePosts as $post) {; ?>
+                    <h1><?php echo $post["title"]; ?></h1>
+                    <h3><?php echo $post["author"]; ?></h3>
+                    <p><?php echo $post["text"]; ?></p>
+                    <?php } ?>
+            </section>
+            <?php
+        }
+        ?>
+    </body>
+</html>
